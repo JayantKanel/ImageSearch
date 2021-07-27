42c6dcef6873670ef3470a4a6adb0adfc879ad23
Jayant Kanel <jayahntkanel@gmail.com>
2021-07-27 14:07:20 +0530

    footer added

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -34,10 +34,8 @@ function Gallery() {
     SetLoadMore(false);
   };
   const onSubmit = async () => {
-    if(term1)
-      setTerm(term1);
-    else 
-      setTerm("Random");
+    if (term1) setTerm(term1);
+    else setTerm("Random");
   };
   useEffect(() => {
     console.log(location.pathname);
@@ -108,65 +106,110 @@ function Gallery() {
         </form>
       </div>
       <div>
-          {
-            term?
-        <div className="justify-content-center">
-        {loading  ? (
-          <div className="d-flex justify-content-center">
-            <div className="spinner-border text-primary" role="status"></div>
-          </div>
-        ) : (
+        {term ? (
           <div className="justify-content-center">
-            {data ? (
-              <div className="container cont">
-                <div className="row">
-                  {data.map((img) => {
-                    const ID = img.id;
-                    return (
-                      <div className="col-md-3 col-sm-4 col-xs-6 img-div justify-content-center">
-                        <a href={`${location.pathname}/${ID}`}>
-                          <img
-                            className="img-responsive img-size"
-                            src={img.urls.regular}
-                            alt="loading..."
-                          />
-                        </a>
-                      </div>
-                    );
-                  })}
-                </div>
-            <div>
-              {loadmore ? (
-                <div className="d-flex justify-content-center spinnerload">
-                  <div
-                    className="spinner-border text-primary"
-                    role="status"
-                  ></div>
-                </div>
-              ) : (
-                <div className="text-center load">
-                  <button
-                    type="button"
-                    className="btn btn-primary"
-                    onClick={Load}
-                  >
-                    Load more Images
-                  </button>
-                </div>
-              )}
-            </div>
+            {loading ? (
+              <div className="d-flex justify-content-center">
+                <div
+                  className="spinner-border text-primary"
+                  role="status"
+                ></div>
               </div>
             ) : (
-              <></>
+              <div className="justify-content-center">
+                {data ? (
+                  <div className="container cont">
+                    <div className="row">
+                      {data.map((img) => {
+                        const ID = img.id;
+                        return (
+                          <div className="col-md-3 col-sm-4 col-xs-6 img-div justify-content-center">
+                            <a href={`${location.pathname}/${ID}`}>
+                              <img
+                                className="img-responsive img-size"
+                                src={img.urls.regular}
+                                alt="loading..."
+                              />
+                            </a>
+                          </div>
+                        );
+                      })}
+                    </div>
+                    <div>
+                      {loadmore ? (
+                        <div className="d-flex justify-content-center spinnerload">
+                          <div
+                            className="spinner-border text-primary"
+                            role="status"
+                          ></div>
+                        </div>
+                      ) : (
+                        <div className="text-center load">
+                          <button
+                            type="button"
+                            className="btn btn-primary"
+                            onClick={Load}
+                          >
+                            Load more Images
+                          </button>
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                ) : (
+                  <></>
+                )}
+              </div>
             )}
           </div>
-        )
-       }
-        </div>
-         :
-         <></>
-}
+        ) : (
+          <></>
+        )}
       </div>
+      <div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+      <footer className="text-center text-white fcolor footer">
+        <div className="container pt-4">
+          <section className="mb-4">
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="https://www.linkedin.com/in/jayant-kanel-669398195/"
+              role="button"
+              data-mdb-ripple-color="dark"
+              target="_blank"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+            <a
+              className="btn btn-link btn-floating btn-lg text-dark m-1"
+              href="https://github.com/JayantKanel"
+              role="button"
+              data-mdb-ripple-color="dark"
+              target="_blank"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+          </section>
+        </div>
+        <div className="text-center text-dark p-3 section">
+          {/* Email: jayantkanel@gmail.com */}
+          <a
+            href="mailto:jayantkanel@gmail.com"
+            target="_blank"
+            className="text-dark"
+          >
+            jayantkanel@gmail.com
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
